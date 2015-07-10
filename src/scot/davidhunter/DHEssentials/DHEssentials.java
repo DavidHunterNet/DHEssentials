a4867ec3ba7b9db6e6db51472f5fef16b6d4324f
David Hunter <contact@david-hunter.net>
2015-07-10 15:07:39 +0100

    Updated the onEnable and onDisable log entries so the plugin logs entries from itself, and therefore has the name auto-prefixed, making the globalChatPrefix no longer needed here, so removed it from the entries. Added Chat Colours to the globalChatPrefix, chat entries using this prefix now look much prettier, but not used anywhere yet.

--- src/scot/davidhunter/DHEssentials/DHEssentials.java
+++ src/scot/davidhunter/DHEssentials/DHEssentials.java
@@ -2,23 +2,24 @@ package scot.davidhunter.DHEssentials;
 
 import java.util.logging.Level;
 
+import org.bukkit.ChatColor;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class DHEssentials extends JavaPlugin {
 
-	private final String globalChatPrefix = "[DHEssentials] ";
+	private final String globalChatPrefix = ChatColor.GOLD + "[" + ChatColor.RED + "DHEssentials" + ChatColor.GOLD + "] " + ChatColor.GRAY;
 	
 	@Override
 	public void onEnable() {
 		
-		this.getServer().getLogger().log(Level.INFO, this.globalChatPrefix + "Plugin Enabled!");
+		this.getLogger().log(Level.INFO, "Plugin Enabled!");
 		
 	}
 	
 	@Override
 	public void onDisable() {
 		
-		this.getServer().getLogger().log(Level.INFO, this.globalChatPrefix + "Plugin Disabled!");
+		this.getLogger().log(Level.INFO, "Plugin Disabled!");
 		
 	}
 	
