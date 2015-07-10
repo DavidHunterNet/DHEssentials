44a346805d56ac3c2dd4b3622f8d70bd1482a223
David Hunter <contact@david-hunter.net>
2015-07-10 16:25:22 +0100

    Created class. Added listeners for PlayerJoinEvent and PlayerQuitEvent. Set custom join and quit messages.

---
+++ src/scot/davidhunter/DHEssentials/PlayerListener.java
@@ -0,0 +1,34 @@
+package scot.davidhunter.DHEssentials;
+
+import net.md_5.bungee.api.ChatColor;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
+
+public class PlayerListener implements Listener {
+
+	private DHEssentials plugin;
+	
+	public PlayerListener(DHEssentials plugin) {
+		
+		this.plugin = plugin;
+		
+	}
+	
+	@EventHandler
+	public void onPlayerJoin(PlayerJoinEvent event) {
+		
+		event.setJoinMessage(plugin.getGlobalChatPrefix() + ChatColor.BOLD + event.getPlayer().getDisplayName() + ChatColor.GRAY + " has joined the server!");
+		
+	}
+	
+	@EventHandler
+	public void onPlayerQuit(PlayerQuitEvent event) {
+		
+		event.setQuitMessage(plugin.getGlobalChatPrefix() + ChatColor.BOLD + event.getPlayer().getDisplayName() + ChatColor.GRAY + " has left the server!");
+		
+	}
+	
+}
