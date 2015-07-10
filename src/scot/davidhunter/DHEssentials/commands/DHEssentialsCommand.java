239c508d85f271e1ea9ab5f43a4cbb04f411724f
David Hunter <contact@david-hunter.net>
2015-07-10 15:47:18 +0100

    Added command usage info. Added /dhe command alias. Added required permission node "dhessentials.dhessentials". Added error for if command sender does not have permission.

--- src/scot/davidhunter/DHEssentials/commands/DHEssentialsCommand.java
+++ src/scot/davidhunter/DHEssentials/commands/DHEssentialsCommand.java
@@ -1,5 +1,9 @@
 package scot.davidhunter.DHEssentials.commands;
 
+import java.util.ArrayList;
+
+import net.md_5.bungee.api.ChatColor;
+
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.defaults.BukkitCommand;
 
@@ -15,12 +19,32 @@ public class DHEssentialsCommand extends BukkitCommand {
 		super(DHEssentialsCommand.NAME);
 		
 		this.plugin = plugin;
+		
+		// COMMAND INFO
+		this.description = "Welcomes you to the plugin.";
+		this.usageMessage = "/dhessentials";
+		
+		// COMMAND ALIASES
+		ArrayList<String> aliases = new ArrayList<String>();
+		
+		aliases.add("dhe");
+		
+		this.setAliases(aliases);
 	}
 
 	@Override
 	public boolean execute(CommandSender sender, String alias, String[] args) {
 		
-		sender.sendMessage(plugin.getGlobalChatPrefix() + "Welcome to the DHEssentials plugin!");
+		if( sender.hasPermission("dhessentials.dhessentials") ) {
+		
+			sender.sendMessage(plugin.getGlobalChatPrefix() + "Welcome to the DHEssentials plugin!");
+			
+		} else {
+			
+			sender.sendMessage(plugin.getGlobalChatPrefix() + ChatColor.DARK_RED + "Sorry, you do not have permission!");
+			sender.sendMessage(plugin.getGlobalChatPrefix() + ChatColor.DARK_RED + "Missing: " + ChatColor.RED + "dhessentials.dhessentials");
+			
+		}
 		
 		return false;
 	}
